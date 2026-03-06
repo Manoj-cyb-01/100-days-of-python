752e1e264f04114a53d0ceb27ec8676ae06ad550
MANOJ KUMAR P <pmanojkumar9972@gmail.com>
2026-03-06 21:07:03 +0530

    Update band_name_generator.py

--- Day-01-Band-Name-Generator/band_name_generator.py
+++ Day-01-Band-Name-Generator/band_name_generator.py
@@ -1,1 +1,5 @@
+print("Welcome to the band Name genrator...")
+city=(input("what's the namee of the city you grew up : "))
+pet_name=(input("what's your pet's name : "))
+print(city + " " + pet_name +" Band's")
 
