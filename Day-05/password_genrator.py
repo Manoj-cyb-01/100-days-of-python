42ab4812c1dbdc4934c5d97bb4a360050b552300
mannoj-cyb-45 <pmanojkumar9972@gmail.com>
2026-05-22 21:27:18 +0530

    woring with day 5

---
+++ Day-05/password_genrator.py
@@ -0,0 +1,4 @@
+print("Welcome to Password Generator")
+no_letters =int(input("Enter the number of letters you would like to add in password: "))
+no_numbers = int(input("Enter the number of numbers you would like to add in password: "))
+no_symbols = int(input("Enter the number of symbols you would like to add in password: "))
